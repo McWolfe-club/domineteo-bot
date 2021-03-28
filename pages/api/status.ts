2c1bb7e83bcbbeeb410beba1f7e78c7dadc80723
Ricardo Montero <rjtm1992@gmail.com>
2021-03-28 21:45:28 +0100

    add key verification

--- pages/api/status.ts
+++ pages/api/status.ts
@@ -1,6 +1,14 @@
-// API for dom bot to check status for snek erth game
+import { verifyKey } from "discord-interactions";
 
+// API for dom bot to check status for snek erth game
 export default (req, res) => {
+    const signature = req.headers['X-Signature-Ed25519'];
+    const timestamp = req.headers['X-Signature-Timestamp'];
+    const isValidRequest = verifyKey(req.rawBody, signature, timestamp, process.env.APP_PUBLIC_KEY);
+    if (!isValidRequest) {
+      return res.status(401).end('Bad request signature');
+    }
+
     res.status(200).json({ 
         type: 4,
         data: {
