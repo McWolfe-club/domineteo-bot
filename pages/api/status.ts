f8a254f0a4f5bfbc0d0e176d0c2c408965facab9
Ricardo Montero <rjtm1992@gmail.com>
2021-03-28 21:52:53 +0100

    transform to lowercase

--- pages/api/status.ts
+++ pages/api/status.ts
@@ -2,8 +2,9 @@ import { verifyKey } from "discord-interactions";
 
 // API for dom bot to check status for snek erth game
 export default (req, res) => {
-    const signature = req.headers['X-Signature-Ed25519'];
-    const timestamp = req.headers['X-Signature-Timestamp'];
+    const signature = req.headers['x-signature-ed25519'];
+    const timestamp = req.headers['x-signature-timestamp'];
+    console.log(signature, timestamp);
     const isValidRequest = verifyKey(req.rawBody, signature, timestamp, process.env.APP_PUBLIC_KEY);
     if (!isValidRequest) {
       return res.status(401).end('Bad request signature');
