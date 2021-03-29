e713a944185d63aa64aec1a5d1922b9f5e795cf5
Ricardo Montero <rjtm1992@gmail.com>
2021-03-29 01:26:31 +0100

    command refactor

--- pages/api/status.ts
+++ pages/api/status.ts
@@ -1,7 +1,8 @@
-import { verifyKey } from 'discord-interactions';
-import { NextApiRequest, NextApiResponse } from 'next';
-import getRawBody from 'raw-body';
-import { InteractionResType, Nation, PlayerController } from './common';
+import { verifyKey } from "discord-interactions";
+import { NextApiRequest, NextApiResponse } from "next";
+import getRawBody from "raw-body";
+import getGameStatus, { PlayerNationStatus } from "../../util/getGameStatus";
+import { InteractionResType } from "./common";
 
 export const config = {
   api: {
@@ -12,47 +13,27 @@ export const config = {
 
 const APP_PUBLIC_KEY = process.env.APP_PUBLIC_KEY;
 
-function buildContentFromNations(nations: Nation[]): string {
-  let pending = '';
-  let unfinished = '';
-
-  nations.forEach(nat => {
-    if (nat.controller == PlayerController.Human && nat.turnplayed !== '2') {
-      const nation = `    - ${nat.name}\n`;
-      if (nat.turnplayed === '0') {
-        pending += nation;
-      } else if (nat.turnplayed === '1') {
-        unfinished += nation;
-      }
-    }
-  });
-
-  return `${unfinished.length ? 'Unfinished:' : ''}
-    ${unfinished.length ? '\`\`\`' : ''}
-    ${unfinished}
-    ${unfinished.length ? '\`\`\`' : ''}
-
-    ${pending.length ? 'Pending:' : ''}
-    ${pending.length ? '\`\`\`' : ''}
-    ${pending}
-    ${pending.length ? '\`\`\`' : ''}
-    `.trim();
+function formatGameStatus(nations: PlayerNationStatus[]) {
+  return nations
+    .filter((nation) => nation.status != 'Done')
+    .map((nation) => `${nation.name} ${nation.status === 'Pending' ? ':alarm_clock:' : ':white_check_mark:'}`)
+    .join('\n');
 }
 
 // API for dom bot to check status for snek erth game
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const signature = (req as any).headers['x-signature-ed25519'];
-  const timestamp = (req as any).headers['x-signature-timestamp'];
+  const signature = (req as any).headers["x-signature-ed25519"];
+  const timestamp = (req as any).headers["x-signature-timestamp"];
   const rawBody = (await getRawBody(req)).toString();
   const isValidRequest = verifyKey(
     rawBody,
     signature,
     timestamp,
-    APP_PUBLIC_KEY,
+    APP_PUBLIC_KEY
   );
 
   if (!isValidRequest) {
-    return res.status(401).end('invalid request signature');
+    return res.status(401).end("invalid request signature");
   }
 
   const jsonBody = JSON.parse(rawBody);
@@ -60,30 +41,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (jsonBody.type === InteractionResType.Pong) {
     res.status(200).json({ type: InteractionResType.Pong });
   } else {
-    const optionId = jsonBody.data.options.find(opt => opt.name === 'id');
-    const response = await fetch(`https://dom5.snek.earth/api/games/${optionId.value}/status`);
+    const optionId = jsonBody.data.options.find((opt) => opt.name === "id");
 
-    if (response.ok) {
-      const nationData: { nations: Nation[] } = await response.json();
+    try {
+      const gameStatus = await getGameStatus(optionId);
+
       const discordJson = {
         type: InteractionResType.ChannelMessageWithSource,
         data: {
           tts: false,
-          content: `
-            Game ${optionId.value}:
-            ${buildContentFromNations(nationData.nations)}
-            `
-        }
+          content: `Game ${optionId}:\n${formatGameStatus}`,
+        },
       };
 
       res.status(200).json(discordJson);
-    } else {
-      res.status(200).json({
+    } catch {
+      return res.status(200).json({
         type: InteractionResType.ChannelMessageWithSource,
         data: {
           tts: false,
-          content: `Cant find game id ${optionId.value}`,
-        }
+          content: `Cant find game id ${optionId}`,
+        },
       });
     }
   }
---
+++ util/getGameStatus.ts
@@ -0,0 +1,33 @@
+import { Nation, PlayerController } from "../pages/api/common";
+
+export interface PlayerNationStatus {
+  name: string;
+  status: 'Pending' | 'Unfinished' | 'Done';
+}
+
+const TURN_PLAYED_TO_STATUS: {[key: string]: PlayerNationStatus['status']} = {
+  '0': 'Pending',
+  '1': "Unfinished",
+  '2': 'Done',
+};
+
+function buildContentFromNations(nations: Nation[]): PlayerNationStatus[] {
+  return nations
+    .filter((nation) => nation.controller == PlayerController.Human)
+    .map(nation => ({
+      name: nation.name,
+      status: TURN_PLAYED_TO_STATUS[nation.turnplayed],
+    }));
+}
+
+export default async function (gameId: string) {
+  const response = await fetch(`https://dom5.snek.earth/api/games/${gameId}/status`);
+  
+  if (response.ok) {
+    const nationData: { nations: Nation[] } = await response.json();
+
+    return buildContentFromNations(nationData.nations);
+  } else {
+    throw new Error('Game id not found');
+  }
+}
