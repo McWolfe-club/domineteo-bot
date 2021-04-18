e3f106302e1da2c928bd3d0b35bf42b877391027
Gavaar <oxspit@gmail.com>
2021-04-18 20:20:59 +0200

    pls 3

--- pages/api/check/[id].ts
+++ pages/api/check/[id].ts
@@ -9,49 +9,43 @@ import send_message from "../discord_commands/send_message";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { id: gameId } = req.query;
-    
     try {
         const cronToken = await login();
         const { jobs } = await jobList(cronToken);
         const gameJob = jobs.find(j => j.title.includes(gameId as string));
-        const channelId = gameJob.title.split('__')[1];
+        const channelId = gameJob?.title.split('__')[1];
+        const currentStatus = await getGameStatus(gameId as string);
 
-        try {
-            const currentStatus = await getGameStatus(gameId as string);
+        // get previous game state and send alert if new turn started
+        if (gameJob) {
+            const history = await jobHistory(cronToken, gameJob.jobId);
+            const identifier = history[0]?.identifier;
+            if (identifier) {
+                const previousStatus = await historyDetail(cronToken, identifier);
+                // find nation that was done or unfinished, and is now pending.
+                const newTurn = previousStatus.find(prevNation => {
+                    const currentNation = currentStatus.find(n => n.name === prevNation.name);
+                    return (prevNation.status === 'Done' || prevNation.status === 'Unfinished') && currentNation.status === 'Pending';
+                }) != null;
 
-            // get previous game state and send alert if new turn started
-            if (gameJob) {
-                const history = await jobHistory(cronToken, gameJob.jobId);
-                const identifier = history[0]?.identifier;
-                if (identifier) {
-                    const previousStatus = await historyDetail(cronToken, identifier);
-                    // find nation that was done or unfinished, and is now pending.
-                    const newTurn = previousStatus.find(prevNation => {
-                        const currentNation = currentStatus.find(n => n.name === prevNation.name);
-                        return (prevNation.status === 'Done' || prevNation.status === 'Unfinished') && currentNation.status === 'Pending';
-                    }) != null;
-
-                    if (newTurn) {
-                        const message = `There's a new turn for game ${gameId}`;
-                        await send_message(message, channelId);
-                    }
+                if (newTurn) {
+                    const message = `There's a new turn for game ${gameId}`;
+                    await send_message(message, channelId);
                 }
             }
-
-            res.status(200).json(currentStatus);
-        } catch (error) {
-            if (error === 'Game id not found') {
-                const cronToken = await login();
-                const { jobs } = await jobList(cronToken);
-                const gameJob = jobs.find(j => j.title.includes(gameId as string));
-
-                await deleteJob(cronToken, gameJob.jobId);
-                throw new Error('Game doesn\'t exist anymore. Automatically unsubscribed');
-            }
-
-            throw new Error('Something went wrong when trying to get the current state of the game.\n' + error.toString());
         }
-    } catch(error) {
-        throw new Error(error);
+
+        res.status(200).json(currentStatus);
+    } catch (error) {
+        if (error === 'Game id not found') {
+            const cronToken = await login();
+            const { jobs } = await jobList(cronToken);
+            const gameJob = jobs.find(j => j.title.includes(gameId as string));
+
+            await deleteJob(cronToken, gameJob.jobId);
+            throw new Error('Game doesn\'t exist anymore. Automatically unsubscribed');
+        }
+
+        throw new Error('Something went wrong when trying to get the current state of the game.\n' + error.toString());
     }
 };
--- pages/api/cron-job.org/config.ts
+++ pages/api/cron-job.org/config.ts
@@ -1,7 +1,9 @@
 export const CRON_URL = 'https://api.cron-job.org/';
 export const CRON_CREDENTIALS = {
-    email: process.env.CRON_LOGIN,
-    password: process.env.CRON_PASSWORD,
+    // email: process.env.CRON_LOGIN,
+    // password: process.env.CRON_PASSWORD,
+    email: 'oxspit@gmail.com',
+    password: 'tFYkya5FYdurBYz'
 };
 
 export enum CronMethod {
