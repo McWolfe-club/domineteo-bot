d20b7e2cb84885720c0558e9ae42509c39175ef3
Gavaar <oxspit@gmail.com>
2021-04-19 02:14:31 +0200

    use proper bot creds

--- pages/api/discord_commands/send_message.ts
+++ pages/api/discord_commands/send_message.ts
@@ -2,7 +2,8 @@ import Discord from 'discord.js';
 
 export default async (message: string, channelId: string): Promise<Discord.Message> => {
     const client = new Discord.Client();
-    await client.login(process.env.PUBLIC_KEY);
+    // await client.login(process.env.DISCORD_LOGIN); // todo: add an env for discord login
+    await client.login('ODI1ODAwMDE1MTIwNTY0MjI0.YGDMMg.xLec0KDJSUIu3uF98rUKdBVuYJI');
 
     try {
         const channel = await client.channels.fetch(channelId) as Discord.TextChannel;
