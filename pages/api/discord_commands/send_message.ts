0c58178e63dcaae989672323fb6d681da4b26164
Gavaar <oxspit@gmail.com>
2021-04-19 02:19:56 +0200

    proper bot token

--- pages/api/discord_commands/send_message.ts
+++ pages/api/discord_commands/send_message.ts
@@ -2,8 +2,7 @@ import Discord from 'discord.js';
 
 export default async (message: string, channelId: string): Promise<Discord.Message> => {
     const client = new Discord.Client();
-    // await client.login(process.env.DISCORD_LOGIN); // todo: add an env for discord login
-    await client.login('ODI1ODAwMDE1MTIwNTY0MjI0.YGDMMg.xLec0KDJSUIu3uF98rUKdBVuYJI');
+    await client.login(process.env.BOT_TOKEN); // todo: add an env for discord login
 
     try {
         const channel = await client.channels.fetch(channelId) as Discord.TextChannel;
